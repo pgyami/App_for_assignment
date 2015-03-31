9bb7dcf64ef03758e5d097ee5535e23c3b6527b1
ngokpika <ngokpika@gmail.com>
2015-03-31 14:23:20 +0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    
    	app/src/main/java/com/example/kuson/app_for_assignment/GameActivity.java
    
    	app/src/main/java/com/example/kuson/app_for_assignment/ResultActivity.java
    	app/src/main/res/layout/activity_game.xml

--- app/src/main/java/com/example/kuson/app_for_assignment/GameActivity.java
+++ app/src/main/java/com/example/kuson/app_for_assignment/GameActivity.java
@@ -1,20 +1,16 @@
 package com.example.kuson.app_for_assignment;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-
-import javax.microedition.khronos.opengles.GL;
+import java.util.Random;
 
 
 public class GameActivity extends ActionBarActivity {
@@ -23,7 +19,7 @@ public class GameActivity extends ActionBarActivity {
     int round_time = (int)(Global_Variable.TOTAL_TIME*1000);
     int clock_tick = (int)(round_time/Global_Variable.MAX_PROGRESSBAR);
     boolean time_cancel = true;
-
+    boolean test = true;
     CountDownTimer mCountDownTimer = new CountDownTimer(round_time, clock_tick) {
 
         @Override
@@ -57,6 +53,26 @@ public class GameActivity extends ActionBarActivity {
         mProgressBar=(ProgressBar)findViewById(R.id.round_time_progressBar);
         mProgressBar.setProgress(i);
 
+        Generate generate_math = GenerateMath(Global_Variable.DIFFICULTY);
+        TextView question_text = (TextView) findViewById(R.id.question_text);
+
+        int gen_number1 = generate_math.getNumber1();
+        int gen_number2 = generate_math.getNumber2();
+        int gen_result=generate_math.getResult();
+
+
+        String text ="";
+
+        if(gen_number1 + gen_number2 == gen_result)
+        {test = true;
+            text = "Đúng";}
+        else{ test = false;
+            text = "Sai";}
+
+        if(generate_math.getNumber2()<0)
+            question_text.setText(gen_number1 + " - " + String.valueOf(gen_number2).substring(1) + " = " + gen_result + "----->" + text);
+        else
+            question_text.setText(gen_number1 + " + " + gen_number2 + " = " + gen_result  + "----->" + text);
 
         getNextLevel();
 
@@ -87,7 +103,9 @@ public class GameActivity extends ActionBarActivity {
     }
 
     public void showResult(View clickedButton){
-        reload();
+
+        if(test == true)reload();   //Finish Page
+         else getNextLevel(); //Reload
     }
 
     private void startTimer(int time, int tick){
@@ -95,13 +113,14 @@ public class GameActivity extends ActionBarActivity {
         mCountDownTimer.start();
         time_cancel = false;
     }
-    private void reload(){
+    private void reload(){                      //Finish Page
+
         mCountDownTimer.cancel();
         Intent activityIntent = new Intent(this, ResultActivity.class);
         startActivity(activityIntent);
     }
 
-    private void getNextLevel(){
+    private void getNextLevel(){                //Reload
         i = 0;
 
        // mProgressBar=(ProgressBar)findViewById(R.id.round_time_progressBar);
@@ -112,12 +131,70 @@ public class GameActivity extends ActionBarActivity {
         level_text.setText(String.valueOf(Global_Variable.LEVEL));
 
         startTimer(3000, 20);
+        Generate generate_math = GenerateMath(Global_Variable.DIFFICULTY);
+        TextView question_text = (TextView) findViewById(R.id.question_text);
+
+        int gen_number1 = generate_math.getNumber1();
+        int gen_number2 = generate_math.getNumber2();
+        int gen_result=generate_math.getResult();
+
+
+        String text ="";
+
+        if(gen_number1 + gen_number2 == gen_result)
+        {test = true;
+            text = "Đúng";}
+        else{ test = false;
+            text = "Sai";}
+
+        if(generate_math.getNumber2()<0)
+            question_text.setText(gen_number1 + " - " + String.valueOf(gen_number2).substring(1) + " = " + gen_result + "----->" + text);
+        else
+            question_text.setText(gen_number1 + " + " + gen_number2 + " = " + gen_result  + "----->" + text);
 
     }
 
     public void showNextLevel(View clickedButton){
         time_cancel = true;
-        getNextLevel();
+        if(test == true) getNextLevel();   //Finish Page
+        else reload(); //Reload
     }
 
+
+    //Generate Math
+    public static Generate GenerateMath(int difficult){
+        int sign;       //SIGN OF NUMBER_2
+        int sign_error; //SIGN OF ERROR
+        int number1;    //A
+        int number2;    //B
+        int result;     //C
+        int error = 0;
+        int randomsize; //randomsize = 20 if diff = 1 ---- randomsize = 30 if diff = 2 ----- else randomsize = 50
+        Random randomizer = new Random();
+
+        if(difficult == 1) randomsize = 20;
+        else if(difficult == 2) randomsize = 30;
+        else randomsize = 50;
+
+
+        int gen_sign = randomizer.nextInt(2);   //IF 0 -> SIGN = + ELSE SIGN = -
+        if(gen_sign==0) sign = 1;
+        else sign = -1;
+
+        //CREATE A AND B
+        number1 = (1+randomizer.nextInt(randomsize));
+        number2 = sign*(1+randomizer.nextInt(randomsize));
+        int test = randomizer.nextInt(2);   //IF 0 -> FALSE // ELSE  -> TRUE
+        if(test==0){
+
+            int gen_sign_x = randomizer.nextInt(2); //IF 0 -> SIGN = + ELSE SIGN = -
+            if(gen_sign_x==0) sign_error = 1;
+            else sign_error = -1;
+            error = sign_error*(1+randomizer.nextInt(3));
+
+        }
+        result = number1 + number2 + error;
+
+        return new Generate(number1,number2,result);
+    }
 }
---
+++ app/src/main/java/com/example/kuson/app_for_assignment/Generate.java
@@ -0,0 +1,29 @@
+package com.example.kuson.app_for_assignment;
+
+/**
+ * Created by asus on 3/31/2015.
+ */
+public class Generate {
+    private int number1;
+    private int number2;
+    private int result;
+
+    public Generate(int a, int b, int c){
+        number1 = a;
+        number2 = b;
+        result = c;
+
+    }
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    public int getResult() {
+        return result;
+    }
+}
