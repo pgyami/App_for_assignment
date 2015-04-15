d1098eb8094f2010d284bcbddbd8b1ccd4969c98
yami <bapts0911@gmail.com>
2015-04-15 15:51:34 +0700

    remodule app + add extra time feature

--- app/src/main/java/com/example/kuson/app_for_assignment/Global_Variable.java
+++ app/src/main/java/com/example/kuson/app_for_assignment/Global_Variable.java
@@ -15,4 +15,5 @@ public class Global_Variable
     public static int MAX_PROGRESSBAR = 100;
     public static double DECREASE_TIME = 0.05;
     public static String CONFIG_FILE_NAME = "config.conf";
+    public static boolean GAME_RUNNING_STATUS = true;
 }
